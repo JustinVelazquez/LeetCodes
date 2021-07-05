2d2f722a3e0d15f2546f366cd3da8600b220699c
Justin V <justinv4994@gmail.com>
2021-07-05 15:53:14 -0400

    codewars

---
+++ amazon/buy-and-sell-stocks/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  //     prices.sort((x,y) => x - y)
+  //    return prices[prices.length - 1] - prices[0]
+
+  let maxprofit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      let profit = prices[j] - prices[i];
+      if (profit > maxprofit) maxprofit = profit;
+    }
+  }
+  return maxprofit;
+};

da009b01a32ea899be8819899c21d3441c476666
Justin V <justinv4994@gmail.com>
2021-07-06 23:06:54 -0400

    leetcode

--- easy/anagrams/index.js
+++ easy/anagrams/index.js
@@ -2,6 +2,7 @@
  * @param {string} s
  * @param {string} t
  * @return {boolean}
+ *  * @param {string} t
  */
 var isAnagram = function (s, t) {
   return s.split('').sort().join('') === t.split('').sort().join('');

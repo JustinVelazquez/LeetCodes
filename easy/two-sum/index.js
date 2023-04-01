13c42ae3891ebb5690a74aa521bb344101c018bb
Justin V <justinv4994@gmail.com>
2023-04-01 17:58:27 -0400

    updated time complexity

--- easy/two-sum/index.js
+++ easy/two-sum/index.js
@@ -3,16 +3,31 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[j] === target - nums[i]) {
-                return [i, j]
-            }
-        } 
+// Following has a Time Complexity of O(N^2) Quadratic
+// var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             if(nums[j] === target - nums[i]) {
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+
+// Following has a T.C of O(N) Linear
+function twoSum(nums, target) {
+  let numObject = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let otherNum = target - nums[i];
+    console.log(numObject, otherNum);
+    if (numObject[otherNum] !== undefined) {
+      return [numObject[otherNum], i];
     }
+    numObject[nums[i]] = i;
+  }
 }
 
-
 // Test Case
-// [2,7,11,15], 9
+// [11,15, 2,7], 9  =>> [2,3]
+console.log(twoSum([11, 15, 2, 7], 9));
